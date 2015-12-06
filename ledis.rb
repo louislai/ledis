9c587a3b501d2a824eed2ead386372e70dcbcf21
Louis Lai <hoangdung2506@gmail.com>
2015-12-06 10:53:57 +0700

    Add all sets and linked list commands

--- ledis.rb
+++ ledis.rb
@@ -1,8 +1,8 @@
 require 'set'
 
-LEDIS_COMMANDS = ['flushdb', 'set', 'get', 'llen', 'rpush',
-                  'lpop', 'rpop', 'lrange', 'del',
-                  'sadd', 'scard', 'srem', 'smembers']
+LEDIS_COMMANDS = ['flushdb', 'set', 'get',
+                  'llen', 'rpush', 'lpop', 'rpop', 'lrange', 'del',
+                  'sadd', 'scard', 'srem', 'smembers', 'sinter']
 
 class Ledis
   attr_accessor :storage, :timestamp
@@ -33,6 +33,7 @@ class Ledis
   end
 
   def sadd(key, *others)
+    return 'EINV' if others.blank?
     @storage[key] = Set.new
     others.each { |other| @storage[key].add other }
     'OK'
@@ -43,7 +44,8 @@ class Ledis
     @storage[key].size
   end
 
-  def srem(key, *other)
+  def srem(key, *others)
+    return 'EINV' if others.blank?
     return 'EKTYP' unless @storage[key]
     others.each { |other| @storage[key].delete other }
     'OK'
@@ -51,10 +53,43 @@ class Ledis
 
   def smembers(key)
     return 'EKTYP' unless @storage[key]
-    @storage[key].to_a.join(' ')
+    @storage[key].to_a.to_json
+  end
+
+  def llen(key)
+    return 'EKTYP' unless @storage[key]
+    @storage[key].size
+  end
+
+  def rpush(key, *others)
+    return 'EINV' if others.blank?
+    @storage[key] ||= []
+    others.each { |other| @storage[key] << other }
+    @storage[key].size
+  end
+
+  def lpop(key)
+    return 'EKTYP' unless @storage[key]
+    @storage[key].shift
+  end
+
+  def rpop(key)
+    return 'EKTYP' unless @storage[key]
+    @storage[key].pop
+  end
+
+  def lrange(key, start, stop)
+    return 'EKTYP' unless @storage[key]
+    @storage[key][start..stop+1].to_json
+  end
+
+  def sinter(*sets)
+    return [].to_json if sets.blank?
+    sets.reduce(:&).to_json
   end
 
   def handle_command(command)
-    send(command[0].downcase, *command[1..-1]) if LEDIS_COMMANDS.include? command[0].downcase
+    return 'ECOM' unless LEDIS_COMMANDS.include? command[0].downcase
+    send(command[0].downcase, *command[1..-1])
   end
 end
